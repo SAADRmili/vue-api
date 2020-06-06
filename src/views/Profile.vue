1a62d3968c0411a89353df04b4371df1ae1262bf
SAADRMILI <saadrmili45@gmail.com>
2020-06-06 12:57:56 +0100

    Add route and login page

---
+++ src/views/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="login">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <h2 class="display-4 text-center mt-5">Profile</h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
