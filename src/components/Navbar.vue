e95d5bd0febfa1e4430f2963d384296c9e671e35
SAADRMILI <saadrmili45@gmail.com>
2020-06-06 12:43:47 +0100

    Add navbar to vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Vue LARAVEL API</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul  class="navbar-nav ml-auto">
+      <li class="nav-item">
+         <a class="btn btn-primary my-1 btn-block" href="#">Login </a> 
+      </li>
+      <li class="nav-item">
+         <a class="btn btn-success my-1 ml-1 btn-block" href="#">Register </a> 
+      </li>
+    </ul>
+   
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
