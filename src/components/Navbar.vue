ea80d7f420c76efe246db124f61e5823a113b3a4
SAADRMILI <saadrmili45@gmail.com>
2020-06-09 21:26:21 +0100

    implement logout  method

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -15,17 +15,25 @@
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
+        <li class="nav-item" v-if="!token">
           <router-link class="btn btn-primary my-1 btn-block" to="/login"
             >Login
           </router-link>
         </li>
-        <li class="nav-item">
+        <li class="nav-item" v-if="!token">
           <router-link
             class="btn btn-success my-1 ml-1 btn-block"
             to="/register"
             >Register
           </router-link>
+        </li>
+        <li class="nav-item" v-if="token">
+          <button
+            class="btn btn-warning my-1 ml-1 btn-block"
+            @click="performLogout"
+          >
+            Logout
+          </button>
         </li>
       </ul>
     </div>
@@ -33,7 +41,29 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      token: null
+    };
+  },
+  mounted() {
+    this.checkUserStatus();
+  },
+  methods: {
+    checkUserStatus() {
+      if (localStorage.getItem("token") != null) {
+        this.token = localStorage.getItem("token");
+      }
+    },
+    performLogout() {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      this.token = null;
+      this.$router.push("/login");
+    }
+  }
+};
 </script>
 
 <style></style>
